62b87ca84f2f35b12d42a7614a7e2f02b6d101bc
dam <dam@AULA005-PORTATIL1>
2022-01-19 09:51:24 +0100

    Se ha creado rama nueva

---
+++ EjerciciioGIt/src/ejerciciiogit/ClaseNueva.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciiogit;
+
+/**
+ *
+ * @author dam
+ */
+public class ClaseNueva {
+    public static void main ( String[] args){
+System.out.println("En la rama. Se crea la clase nueva");
+}
+}
